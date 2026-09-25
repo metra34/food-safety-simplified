c3253e938363424119b0d68ef13e76c294c87382
Artem Clement <artemcodes.dev@gmail.com>
2026-09-25 18:40:07 -0400

    project structure change

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-import logo from "./assets/logo.png"
+import logo from "@/app/assets/logo.png"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
